f960056385d7bbacf2febfaa777cace4b28de7a5
k-murakami <k.murakami.jst@gmail.com>
2025-06-30 09:48:24 +0900

    log出力機能追加時のコミット

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.todo.app.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +15,24 @@ import com.todo.app.mapper.TodoMapper;
 
 @Controller
 public class TodoController {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
+	
 	@Autowired
 	TodoMapper todoMapper;
 
 	@RequestMapping(value="/")
 	public String index(Model model) {
+		logger.info("アクセス： /");
 
 //		List<Todo> list = todoMapper.selectAll();
 
 		List<Todo> list = todoMapper.selectIncomplete();
 		List<Todo> doneList = todoMapper.selectComplete();
+		
+		logger.debug("list:" + list.size() + "件");
+		logger.debug("doneList:" + doneList.size() + "件");
+		
 		model.addAttribute("todos",list);
 		model.addAttribute("doneTodos",doneList);
 
@@ -33,6 +42,8 @@ public class TodoController {
 	@RequestMapping(value="/add")
 	@ResponseBody
 	public Todo add(Todo todo) {
+		logger.info("アクセス：/add");
+		
 		todoMapper.add(todo);
 		return todo;
 	}
@@ -40,12 +51,16 @@ public class TodoController {
 	@RequestMapping(value="/update")
 	@ResponseBody
 	public void update(Todo todo) {
+		logger.info("アクセス：/update");
+		
 		todoMapper.update(todo);
 	}
 
 	@RequestMapping(value="/delete")
 	@ResponseBody
 	public void delete() {
+		logger.info("アクセス：/delete");
+		
 		todoMapper.delete();
 	}
 
